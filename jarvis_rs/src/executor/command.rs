90abe0998b46c9c27540d12c6c8162954c840cca
v1shay <v.agrwl17@gmail.com>
2026-03-04 18:40:06 -0800

    initialize core execution engine

--- jarvis_rs/src/executor/command.rs
+++ jarvis_rs/src/executor/command.rs
@@ -1,6 +1,53 @@
-use anyhow::Result;
+use anyhow::{Result, bail};
+use std::process::Command;
+use crate::intent::validator::ValidatedParams;
 
-pub async fn execute_safe(_cmd: &str, _args: &[String]) -> Result<()> {
-    // TODO: implement allowlist/denylist, sandboxing, and safe process spawning.
-    anyhow::bail!("executor not implemented")
+pub fn execute(intent: ValidatedParams) -> Result<()> {
+    match intent {
+
+        ValidatedParams::OpenApplication(params) => {
+            println!("Opening application: {}", params.app_name);
+
+            Command::new("open")
+                .arg("-a")
+                .arg(params.app_name)
+                .spawn()?;
+        }
+
+        ValidatedParams::ListFiles(params) => {
+            println!("Listing files in: {}", params.directory);
+
+            Command::new("ls")
+                .arg(params.directory)
+                .spawn()?;
+        }
+
+        ValidatedParams::CreateFolder(params) => {
+            println!("Creating folder: {}/{}", params.directory, params.folder_name);
+
+            Command::new("mkdir")
+                .arg(format!("{}/{}", params.directory, params.folder_name))
+                .spawn()?;
+        }
+
+        ValidatedParams::DeleteFile(params) => {
+            println!("Deleting file: {}", params.path);
+
+            Command::new("rm")
+                .arg(params.path)
+                .spawn()?;
+        }
+
+        ValidatedParams::SearchWeb(params) => {
+            println!("Searching web: {}", params.query);
+
+            let url = format!("https://www.google.com/search?q={}", params.query);
+
+            Command::new("open")
+                .arg(url)
+                .spawn()?;
+        }
+    }
+
+    Ok(())
 }
